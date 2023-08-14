8159079169eb22a762100331214d08b879cfa367
Vinodh Kumar <vinodhkumar.manickam@freshworks.com>
2023-08-14 22:05:18 +0530

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: { alias: { src: resolve('src/') } },
+});
